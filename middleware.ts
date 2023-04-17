822f81b1772db4fd8cff06ac0162841b48f44b88
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-04-17 12:46:05 -0300

    feat(supa): add middleware to handle auth stuff

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,4 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareSupabaseClient({ req, res });
   await supabase.auth.getSession();
-  return res;
 }
